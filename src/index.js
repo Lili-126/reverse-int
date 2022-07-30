562af4bebfe5ef135bf03479d20a1d3cb0d677ed
Liliya <gabdulahatov_na@mail.ru>
2022-07-30 18:58:04 +0500

    Feat:  Solving the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
-}
+        return (
+        parseFloat(
+          n
+            .toString()
+            .split('')
+            .reverse()
+            .join('')
+        )
+        //  ) * Math.sign(n)
+      )
+    }
+
+
